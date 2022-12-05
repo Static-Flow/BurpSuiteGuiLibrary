34503e984a02eeacd1e71a35cfbbc2550fd9e507
Static-Flow <tanner989@gmail.com>
2022-12-04 21:40:30 -0600

    stable release

--- src/main/java/com/staticflow/BurpGuiControl.java
+++ src/main/java/com/staticflow/BurpGuiControl.java
@@ -1,133 +1,136 @@
-package com.staticflow;
+package main.java.com.staticflow;
 
 import javax.swing.*;
 import java.awt.*;
-import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.List;
 
-/*
-This stores our state for the extension as a Singleton
+/**
+This Library provides a set of tools for easily manipulating the Swing Components that comprise Burp Suite's UI.
  */
-class BurpGuiControl {
+public final class BurpGuiControl {
 
+    /**
+     * This method returns a Component at a specified walk from a starting Component.
+     * For example, take a JFrame named foo, has a JPanel child bar, which itself has two components a JButton baz and a JTextField buz.
+     * The Swing structure would be:
+     * <pre>
+     * foo (JFrame)
+     *  |
+     *  |> bar (JPanel)
+     *      |
+     *      |> baz (JButton)
+     *      |> buz (JTextField)
+     *  |
+     *  |> ... More Components
+     * </pre>
+     * Then to access it using `getComponentAtPath` would be: <pre>BurpGuiControl.getComponentAtPath(foo, new int[]{0,1})</pre>
+     *
+     * Note: Components are 0 indexed. The helper method {@link #printChildrenComponents(Container, int, int) printChildrenComponents} can be used
+     * to print a formatted Swing tree to easily discovered the needed path to an element
+     * @param startingContainer The parent Component to begin walking the user supplied path
+     * @param componentChildPath The indexes to follow down the Swing tree to the desired Component
+     * @return the Component referenced by the user supplied Swing path
+     */
+    public static Component getComponentAtPath(Container startingContainer, int[] componentChildPath) {
+        Container endingContainer = (Container) startingContainer.getComponent(componentChildPath[0]);
+        for(int index = 1; index < componentChildPath.length;index++) {
+            endingContainer = (Container) endingContainer.getComponent(componentChildPath[index]);
+        }
+        return endingContainer;
+    }
 
-    public static void printChildrenComponents(Container component, int level, int maxDepth) {
+    /**
+     * This method recursively walks the subcomponent tree of a provided Component down to a user supplied level and builds a formatted tree.
+     * This allows for targeted use of the other methods in this Library, such as {@link #getComponentAtPath(Container, int[]) getComponentAtPath}
+     * which requires knowledge of the direct path to a child component from a given parent.
+     * @param component The parent component that will be recursively walked
+     * @param level used for the recursive algorithm to track how many levels down the walk has traveled
+     * @param maxDepth The lowest depth to search for children. Level 1 would be children only, level 2 would include grandchildren, and so on
+     */
+    public static String printChildrenComponents(Container component, int level, int maxDepth) {
+        StringBuilder prettyPrintSwingTree = new StringBuilder();
         StringBuilder tabs = new StringBuilder();
         for(int i=0;i<level;i++){
             tabs.append("\t");
         }
         int count = 0;
         for (Component child : component.getComponents()){
-            System.out.println(tabs.toString()+" "+level+"."+count+"|->"+child);
+            prettyPrintSwingTree.append(String.format("%s %d.%d|->%s%n",tabs,level,count,child));
+            prettyPrintSwingTree.append(System.lineSeparator());
             count++;
             if (child instanceof Container && level != maxDepth) {
-
-                printChildrenComponents((Container) child,level+1, maxDepth);
+                prettyPrintSwingTree.append(printChildrenComponents((Container) child,level+1, maxDepth));
             }
         }
+        return prettyPrintSwingTree.toString();
     }
 
-    public static void printChildrenComponentsInputMaps(Container component, int level, int maxDepth) {
-        StringBuilder tabs = new StringBuilder();
-        for(int i=0;i<level;i++){
-            tabs.append("\t");
-        }
-        try {
-            if (((JComponent) component).getInputMap() != null && ((JComponent) component).getInputMap().allKeys() != null) {
-                for (KeyStroke k : ((JComponent) component).getInputMap().allKeys()) {
-                    if(k.toString().startsWith("shift ctrl pressed") || k.toString().startsWith("ctrl shift pressed")) {
-                        System.out.println(tabs.toString() + " " + component);
-                        System.out.println(tabs.toString() + " " + k + " " + ((JComponent) component).getInputMap().get(k));
-                    }
-                }
-            }
-        } catch (Exception ignored) {
-
-        }
-        for (Component child : component.getComponents()) {
-            if (child instanceof Container && level != maxDepth) {
-
-                printChildrenComponentsInputMaps((Container) child, level + 1, maxDepth);
-            }
-        }
-    }
-
-    public static Container getRootPane(){
+    /**
+     * This method returns the major Swing component of Burp Suite, the JTabbedPane containing the major tabs.
+     * @return The JTabbedPane which holds all other Burp Suite components
+     */
+    public static JTabbedPane getMainTabbedPane(){
         for(Frame frame : Frame.getFrames()) {
             if(frame.isVisible() && frame.getTitle().startsWith(("Burp Suite")))
             {
-                Container rootpane = (Container) frame.getComponent(0);
-                Container layeredPane = (Container) rootpane.getComponent(1);
-                Container jpanel = (Container) layeredPane.getComponent(0);
-                return (Container) jpanel.getComponent(0);
+                return (JTabbedPane) findFirstComponentOfType(frame,JTabbedPane.class);
             }
         }
         return null;
-        //return ExtensionState.getInstance().getLibraryPanelTab().getMainBurpPane();
-    }
-
-
-    public static Component getCurrentlySelectedRepeaterTab() {
-        Container repeaterTabContainer = (Container) getBaseBurpComponent("Repeater");
-        return ((JTabbedPane)repeaterTabContainer).getSelectedComponent();
-    }
-
-    public static Component[] getAllRepeaterTabs() {
-        Container repeaterTabContainer = (Container) getBaseBurpComponent("Repeater");
-        return ((JTabbedPane)repeaterTabContainer).getComponents();
     }
 
     /**
-     * Retrieves the Proxy HTTP History Tab
-     * @return The Proxy HTTP History Tab
+     * This method recursively searches all children of a source Component for a Component matching the provided Component class or Null if one cannot be found
+     * @param parentContainer Starting container to recursively search children
+     * @param clazz The java.awt.Component class which a child Component must match
+     * @return the first child of the parentContainer that matches the clazz java.awt.Component
      */
-    public static Component getProxyHTTPHistoryComponent() {
-        Container proxyTabContainer = (Container) getBaseBurpComponent("Proxy");
-        JTabbedPane tabber = (JTabbedPane) proxyTabContainer;
-        return tabber.getComponentAt(tabber.indexOfTab("HTTP history"));
+    public static Component findFirstComponentOfType(Container parentContainer, Class<? extends java.awt.Component> clazz) {
+        for(Component comp : parentContainer.getComponents()) {
+            if(clazz.isAssignableFrom(comp.getClass())){
+                return comp;
+            } else {
+                Component result = findFirstComponentOfType((Container)comp, clazz);
+                if(result != null && clazz.isAssignableFrom(result.getClass())){
+                    return result;
+                }
+            }
+        }
+        return null;
     }
 
     /**
-     * Retrieves the Proxy Intercept Tab
-     * @return The Proxy Intercept Tab
+     * This method recursively searches all children of a source Component for all Components matching the provided Component class
+     * @param parentContainer Starting container to recursively search children
+     * @param clazz The java.awt.Component class which a child Component must match
+     * @return all children of the parentContainer that match the clazz java.awt.Component
      */
-    public static Component getProxyInterceptComponent() {
-        Container proxyTabContainer = (Container) getBaseBurpComponent("Proxy");
-        return proxyTabContainer.getComponents()[1];
+    public static List<Component> findAllComponentsOfType(Container parentContainer, Class<? extends java.awt.Component> clazz) {
+        ArrayList<Component> matchingComponents = new ArrayList<>();
+        for(Component comp : parentContainer.getComponents()) {
+            if(clazz.isAssignableFrom(comp.getClass())){
+                matchingComponents.add(comp);
+            } else {
+                matchingComponents.addAll(findAllComponentsOfType((Container)comp, clazz));
+            }
+        }
+        return matchingComponents;
     }
 
     /**
-     * Retrieves the Proxy Websocket History Tab
-     * @return The Proxy Websocket History Tab
-     */
-    public static Component getProxyWebsocketHistoryComponent() {
-        Container proxyTabContainer = (Container) getBaseBurpComponent("Proxy");
-        return proxyTabContainer.getComponents()[3];
-    }
-
-    /**
-     * Retrieves the Tab component from BurpSuite denoted by the tab name. For instance if you wanted to Repeater tab
-     * it would be getBaseBurpComponent("Repeater"). This also works for custom tabs generated by extensions.
+     * Retrieves the Tab component from BurpSuite denoted by the tab name. For instance if you wanted the Repeater tab
+     * it would be <pre>getBaseBurpComponent("Repeater")</pre>. This also works for custom tabs generated by extensions.
      * @return The Burp UI tab denoted by the tab name
      */
     public static Component getBaseBurpComponent(String tabName) {
-        JTabbedPane mainTabs =  ((JTabbedPane) getRootPane());
-        //mainTabs.setSelectedIndex(mainTabs.indexOfTab(tabName));
-        //printChildrenComponents((Container)httpHistoryTab,0);
-        //return mainTabs.getSelectedComponent();
-        return mainTabs.getComponentAt(mainTabs.indexOfTab(tabName));
+        JTabbedPane mainTabs =  getMainTabbedPane();
+        if(mainTabs != null) {
+            return mainTabs.getComponentAt(mainTabs.indexOfTab(tabName));
+        } else {
+            return null;
+        }
 
     }
-
-    public static void addBaseBurpComponent(String tabName, Component newComponent) {
-        JTabbedPane mainTabs =  ((JTabbedPane) getRootPane());
-        mainTabs.addTab(tabName,newComponent);
-    }
-
-    public static void replaceBaseBurpComponent(String tabName, Component newComponent) {
-        JTabbedPane mainTabs =  ((JTabbedPane) getRootPane());
-        mainTabs.setComponentAt(mainTabs.indexOfTab(tabName),newComponent);
-    }
-
-
 
 }
